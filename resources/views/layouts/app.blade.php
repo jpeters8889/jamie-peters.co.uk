d0b10d184377f5c6fa716b41df33a93936d31ef7
Jamie Peters <=>
2020-05-11 19:19:03 +0100

    add analytics

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,9 +7,10 @@
     @include('page-view-builder::header')
 
     <link rel="stylesheet" href="{{ mix('assets/css/app.css') }}"/>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/atelier-heath-dark.min.css" />
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/9.12.0/styles/atelier-heath-dark.min.css"/>
 
-    <link rel="icon" href="{{ asset('/assets/favicon.ico') }}" type="image/x-icon" />
+    <link rel="icon" href="{{ asset('/assets/favicon.ico') }}" type="image/x-icon"/>
 
     <script src="{{ mix('/assets/js/manifest.js') }}"></script>
     <script src="{{ mix('/assets/js/vendor.js') }}"></script>
@@ -18,20 +19,32 @@
 <body class="bg-gray-300 min-h-screen">
 
 <div id="app" class="flex flex-col min-h-screen">
-    <x-page-header />
+    <x-page-header/>
 
     <div class="main-wrapper flex-1">
         @yield('content')
     </div>
 
-    <x-page-footer />
+    <x-page-footer/>
 
     <portal-target name="modal"></portal-target>
 </div>
 
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110429316-1"></script>
 <script>
     app().build();
+
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+
+    gtag('js', new Date());
+
+    gtag('config', 'UA-110429316-1');
 </script>
+
 
 </body>
 </html>
